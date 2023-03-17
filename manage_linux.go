cb494ee317500ad8bce19e17f05965bc3fce0e1c
Ajit <zlksnkwork@gmail.com>
2023-03-18 02:31:42 +0530

    feat(linux): make sure the service has not been mistakenly systemctl-disabled

--- manage_linux.go
+++ manage_linux.go
@@ -26,7 +26,13 @@ func manageService(name string, action serviceAction) error {
 		return cli.Exit("This service is unavailable. Install it first before you act upon it.", exitServiceNotPresent)
 	}
 
-	var cmd *exec.Cmd
+	// We've intentionally used the word start/stop over enable/disable because the
+	// latter in our mind means load up the service and enable it or disable the service and unload it.
+	// This is not we want. We expect telltail to be intentionally disabled by the user for some period of time, not permanently.
+	// That's why we'll make sure that telltail loads up on boot by `systemctl enable`-ing the service
+	cmd := exec.Command("systemctl", "--user", "enable", "tellail-"+name)
+	cmd.Run()
+
 	switch action {
 	case startService:
 		cmd = exec.Command("systemctl", "--user", "start", "telltail-"+name)
@@ -36,5 +42,6 @@ func manageService(name string, action serviceAction) error {
 		cmd = exec.Command("systemctl", "--user", "restart", "telltail-"+name)
 	}
 	cmd.Run()
+
 	return nil
 }
