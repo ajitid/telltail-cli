82337bb6b314637569da3af72de6f4683706b595
Ajit <zlksnkwork@gmail.com>
2023-03-18 16:32:30 +0530

    feat: start exit status from 5
    ...to not to step onto the range of exit statuses used by cli pkg

--- exitcodes.go
+++ exitcodes.go
@@ -1,9 +1,13 @@
 package main
 
-// do (line num - 4) to get the exit code number
+// do (line num - 5) to get the exit status number
 const (
-	// starts from 2 because generic errors from cli pkg gives 1 as exit status (errors like missing cli arg, or the program returned an error that is not from cli.Exit())
-	exitUnsupportedOsVariant = iota + 2
+	// --- Intentionally Left Empty (to make calculating exit status using line number easy)
+	// ---
+	// ---
+	// starts from 5 because generic errors from cli pkg gives 1 as exit status (errors like missing cli arg, or the program returned an error that is not from cli.Exit())
+	// and unknown command gives 3
+	exitUnsupportedOsVariant = iota + 5
 	exitFileNotReadable
 	exitFileNotModifiable // In rwx, w includes creation, deletion and makings edits. That's why we've used a word similar to writeable.
 	exitDirNotModifiable
